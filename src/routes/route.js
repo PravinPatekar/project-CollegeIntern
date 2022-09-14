0afd5291df9ea17a3cd8a6cb14c13603c8a36eb5
shikha <shikha1081998@gmail.com>
2022-09-14 12:14:24 +0530

    POST COLLEGE API DONE

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const {createCollege} = require('../controllers/collegeController')
 
+
+
+router.post('/functionup/colleges',createCollege)
 
 
 
