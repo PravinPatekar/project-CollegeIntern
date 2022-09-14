ee2bfb6aeea62eb6b46890231c38c4541e1b78b5
Pravin <pravinpatekar1999@gmail.com>
2022-09-14 11:20:55 +0530

    model done

---
+++ src/models/internModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const internSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    mobile: { type: String, required: true, unique: true, trim: true },
+    collegeId: { type: ObjectId, ref: "college" },
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("intern", internSchema);
